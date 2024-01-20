539d397f9b0722a2d65e672f3295f03aeb3e0d6c
Tan Yunliang (Sky Livingston) <skypad123@gmail.com>
2024-01-20 22:14:50 +0800

    find device id

--- client/roomsense2_client/sensors/audio.py
+++ client/roomsense2_client/sensors/audio.py
@@ -35,5 +35,14 @@ class RpiMic:
         wavefile.close()
 
 if __name__ == "__main__":
-    mic = RpiMic(file_location="audio.wav")
-    mic.capture()
+
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+
+    for i in range(0, numdevices):
+        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+            
+    # mic = RpiMic(file_location="temp/audio.wav")
+    # mic.capture(record_len=60)
