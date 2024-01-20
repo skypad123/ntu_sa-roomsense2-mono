bba114a5a64c676f044bd1f9f758cae8fff5ad5a
Tan Yunliang (Sky Livingston) <skypad123@gmail.com>
2024-01-20 20:26:19 +0800

    update change README to all caps

--- client/sensors/humidity_temp_htu2x.py
+++ client/sensors/humidity_temp_htu2x.py
@@ -1,20 +1,20 @@
 
 from dataclasses import dataclass
-# import board
-# from adafruit_htu21d import HTU21D
-# import asyncio
+import board
+from adafruit_htu21d import HTU21D
+import asyncio
 
 @dataclass
 class HTU21DReading:
     temperature : float
     humidity: float
 
-# async def read_htu21d(i2c_bus) -> HTU21DReading:
-#     htu21d = HTU21D(i2c_bus)
-#     print("Temperature: %0.1f C" % htu21d.temperature)
-#     print("Humidity: %0.1f %%" % htu21d.relative_humidity)
-#     return HTU21DReading(temperature=htu21d.temperature, humidity=htu21d.relative_humidity)
+async def read_htu21d(i2c_bus) -> HTU21DReading:
+    htu21d = HTU21D(i2c_bus)
+    print("Temperature: %0.1f C" % htu21d.temperature)
+    print("Humidity: %0.1f %%" % htu21d.relative_humidity)
+    return HTU21DReading(temperature=htu21d.temperature, humidity=htu21d.relative_humidity)
 
-# if __name__ == "__main__":
-#     i2c = board.I2C()
-#     asyncio.run(read_htu21d(i2c))
+if __name__ == "__main__":
+    i2c = board.I2C()
+    asyncio.run(read_htu21d(i2c))
