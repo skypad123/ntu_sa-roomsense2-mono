38b72930d980943b8acc2fb9219db2bc0d0fd671
Tan Yunliang (Sky Livingston) <skypad123@gmail.com>
2024-01-20 22:31:55 +0800

    test file

--- client/roomsense2_client/sensors/audio.py
+++ client/roomsense2_client/sensors/audio.py
@@ -4,7 +4,7 @@ import wave
 class RpiMic: 
 
     def __init__(self, format= pyaudio.paInt16, \
-    device_index = 0, audio_channel = 1 , samp_rate = 44100, \
+    device_index = 0, audio_channel = 2 , samp_rate = 44100, \
     chunk = 1024, file_location = 'temp/audio.wav'):
         self.format = format
         self.device_index = device_index
@@ -45,5 +45,49 @@ if __name__ == "__main__":
     #     if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
     #         print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
             
-    mic = RpiMic(file_location="temp/audio.wav")
-    mic.capture(record_len=10)
+    # mic = RpiMic(file_location="temp/audio.wav")
+    # mic.capture(record_len=10)
+
+
+ 
+    chunk = 1024      # Each chunk will consist of 1024 samples
+    sample_format = pyaudio.paInt16      # 16 bits per sample
+    channels = 2      # Number of audio channels
+    fs = 44100        # Record at 44100 samples per second
+    time_in_seconds = 3
+    filename = "soundsample.wav"
+    
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    
+    print('-----Now Recording-----')
+    
+    #Open a Stream with the values we just defined
+    stream = p.open(format=sample_format,
+                    channels = channels,
+                    rate = fs,
+                    frames_per_buffer = chunk,
+                    input = True)
+    
+    frames = []  # Initialize array to store frames
+    
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * time_in_seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+    
+    # Stop and close the Stream and PyAudio
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    
+    print('-----Finished Recording-----')
+    
+    # Open and Set the data of the WAV file
+    file = wave.open(filename, 'wb')
+    file.setnchannels(channels)
+    file.setsampwidth(p.get_sample_size(sample_format))
+    file.setframerate(fs)
+    
+    #Write and Close the File
+    file.writeframes(b''.join(frames))
+    file.close()
