a3f79980f7109ee89a097c1163c9d5a01dc1de1e
Tan Yunliang (Sky Livingston) <skypad123@gmail.com>
2024-01-21 19:30:11 +0800

    recording test

--- client/roomsense2_client/sensors/audio.py
+++ client/roomsense2_client/sensors/audio.py
@@ -48,46 +48,45 @@ if __name__ == "__main__":
     # mic = RpiMic(file_location="temp/audio.wav")
     # mic.capture(record_len=10)
 
-
  
-    # chunk = 1024      # Each chunk will consist of 1024 samples
-    # sample_format = pyaudio.paInt16      # 16 bits per sample
-    # channels = 2      # Number of audio channels
-    # fs = 44100        # Record at 44100 samples per second
-    # time_in_seconds = 3
-    # filename = "soundsample.wav"
+    chunk = 1024      # Each chunk will consist of 1024 samples
+    sample_format = pyaudio.paInt16      # 16 bits per sample
+    channels = 2      # Number of audio channels
+    fs = 44100        # Record at 44100 samples per second
+    time_in_seconds = 10
+    filename = "soundsample.wav"
     
-    # p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
     
-    # print('-----Now Recording-----')
+    print('-----Now Recording-----')
     
-    # #Open a Stream with the values we just defined
-    # stream = p.open(format=sample_format,
-    #                 channels = channels,
-    #                 rate = fs,
-    #                 frames_per_buffer = chunk,
-    #                 input = True)
+    #Open a Stream with the values we just defined
+    stream = p.open(format=sample_format,
+                    channels = channels,
+                    rate = fs,
+                    frames_per_buffer = chunk,
+                    input = True,input_device_index=0)
     
-    # frames = []  # Initialize array to store frames
+    frames = []  # Initialize array to store frames
     
-    # # Store data in chunks for 3 seconds
-    # for i in range(0, int(fs / chunk * time_in_seconds)):
-    #     data = stream.read(chunk)
-    #     frames.append(data)
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * time_in_seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
     
-    # # Stop and close the Stream and PyAudio
-    # stream.stop_stream()
-    # stream.close()
-    # p.terminate()
+    # Stop and close the Stream and PyAudio
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
     
-    # print('-----Finished Recording-----')
+    print('-----Finished Recording-----')
     
-    # # Open and Set the data of the WAV file
-    # file = wave.open(filename, 'wb')
-    # file.setnchannels(channels)
-    # file.setsampwidth(p.get_sample_size(sample_format))
-    # file.setframerate(fs)
+    # Open and Set the data of the WAV file
+    file = wave.open(filename, 'wb')
+    file.setnchannels(channels)
+    file.setsampwidth(p.get_sample_size(sample_format))
+    file.setframerate(fs)
     
-    # #Write and Close the File
-    # file.writeframes(b''.join(frames))
-    # file.close()
+    #Write and Close the File
+    file.writeframes(b''.join(frames))
+    file.close()
