b2450f0bdb2cf938cdf9158dbafcd65bd881cfd5
Tan Yunliang (Sky Livingston) <skypad123@gmail.com>
2024-01-22 18:29:16 +0800

    set logging to info

--- client/roomsense2_client/main.py
+++ client/roomsense2_client/main.py
@@ -33,7 +33,7 @@ root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(CustomFormatter())
 root.addHandler(handler)
